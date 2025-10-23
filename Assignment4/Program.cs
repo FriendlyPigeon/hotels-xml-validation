932cb502683b9aea0c222995f6e0a75044f502b9
FriendlyPigeon <levi.liukkonen@gmail.com>
2025-10-23 15:03:16 -0700

    Initial commit of assignment with Hotels schema and XML file

---
+++ Assignment4/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static string xmlURL = "";
+        public static string xmlErrorURL = "";
+        public static string xsdURL = "";
+
+        public static void Main(string[] args)
+        {
+        }
+
+        public static string Verification(string xmlUrl, string xsdUrl)
+        {
+
+        }
+
+        public static string Xml2Json(string xmlUrl)
+        {
+
+        }
+    }
+}
